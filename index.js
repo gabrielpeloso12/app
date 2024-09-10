c9e5ffe39e7e7af25a38245d9afb736eaf935aab
Gabriel Peloso <gabriel_peloso@hotmail.com>
2024-09-09 21:57:22 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//arrays
-let metas = ["gabriel", "alo"]
-console.log(metas[1], metas[0])  
-
-//objects
 let meta = {
     value: 'ler um livro por mês',
     checked: false, 
-    log: (info) => {
-        console.log(info)
-    }
 }
-meta.log(meta.value)
+
+let metas = [
+    meta,
+    {
+        value: 'fazer exercícios físicos',
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
 
